6c8a80bfec90241a41564ebac75b61a376f39353
bruzzo12 <ruzzo.b@northeastern.edu>
2021-04-05 10:58:37 -0400

    Fixed test class

---
+++ test/IModelImplTest.java
@@ -0,0 +1,43 @@
+
+import org.junit.Test;
+
+import model.Circle;
+import model.IModel;
+import model.IModelImpl;
+import model.Rectangle;
+import model.Shape;
+
+
+public class IModelImplTest {
+  IModel t = new IModelImpl();
+  Shape c = new Circle(3, 1,2, 100,100,100,
+          0, 100, "First Circle");
+  Shape r = new Rectangle(2, 3, 1, 1,
+          200, 200, 200,
+          20, 50, "First Rectangle");
+
+  @Test
+  public void addShape() {
+    t.addShape(c);
+  }
+
+  @Test
+  public void removeShape() {
+  }
+
+  @Test
+  public void getSpecificShape() {
+  }
+
+  @Test
+  public void testToString() {
+  }
+
+  @Test
+  public void getShapesAtTicker() {
+  }
+
+  @Test
+  public void getShapeCount() {
+  }
+}
